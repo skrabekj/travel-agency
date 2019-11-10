b0ae42b089b8e117e4959567065c735a838fae87
Jan Skrabek <jan.skrabek@tutanota.com>
2019-11-10 21:01:22 +0100

    added orderoptiontext

---
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+
+const OrderOptionText = ({ setOptionValue }) => (
+  <input
+    className={styles.input}
+    type="text"
+    onChange={event => setOptionValue(event.currentTarget.value)} />
+);
+
+OrderOptionText.propTypes = {
+  setOptionValue: PropTypes.func,
+};
+
+export default OrderOptionText;
