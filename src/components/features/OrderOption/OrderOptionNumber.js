f4c820ae15ce321a8ed0bfd4006a35c2ee4a0078
Jan Skrabek <jan.skrabek@tutanota.com>
2019-11-12 21:53:34 +0100

    add Hero.test

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,12 +1,13 @@
-import React from 'react';
+/*import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './OrderOption.scss';
 
-const OrderOptionNumber =  props => (
+const OrderOptionNumber =  (props, {setOptionValue}) => (
   <div className={styles.number}>
     <input className={styles.inputSmall} type='number'
       value={props.currentValue} min={props.limits.min} max={props.limits.max}
-      onChange={() => props.setOptionValue(props.currentTarget.value.id)} />
+      onChange={() => setOptionValue(props.currentTarget.value.id)} />
+
   </div>
 );
 
@@ -16,6 +17,42 @@ OrderOptionNumber.propTypes = {
   currentValue: PropTypes.string,
   setOptionValue: PropTypes.func,
   currentTarget: PropTypes.node,
+  //setOptionValue: PropTypes.func,
+};
+
+export default OrderOptionNumber;*/
+//onChange={() => props.setOptionValue(props.currentTarget.value.id)} />
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+
+const OrderOptionNumber = ({
+  currentValue,
+  setOptionValue,
+  limits: { min, max },
+}) => {
+  return (
+    <div className={styles.number}>
+      <input
+        className={styles.inputSmall}
+        type="number"
+        value={currentValue}
+        onChange={event => setOptionValue(event.currentTarget.value)}
+        min={min}
+        max={max}
+      />{' '}
+    </div>
+  );
+};
+
+OrderOptionNumber.propTypes = {
+  currentValue: PropTypes.node,
+  setOptionValue: PropTypes.func,
+  tripCost: PropTypes.string,
+  limits: PropTypes.shape({
+    min: PropTypes.number,
+    max: PropTypes.number,
+  }),
 };
 
 export default OrderOptionNumber;
